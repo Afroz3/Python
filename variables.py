1b026a8285a01ffeecea4784142e3fcbd26b8d84
Afroz3 <afrozmohammad1915@gmail.com>
2025-12-24 11:55:38 +0530

    Add variables.py file

---
+++ variables.py
@@ -0,0 +1,21 @@
+#creating a variable
+x =10
+print(x)
+#casting
+y=str(3)
+z=float(6)
+print(y)
+print(z)
+#to get the type of data type
+print(type(x))
+#string values are given in single or double quotes
+name = 'john'
+output = "hello"
+print(name)
+print(output)
+#case sensitive
+#if you create a variable with a it gives different value than  A
+a =4
+A = 5
+print(a)
+print(A)
