2cbf170d91b70a4f6772e74bba8b0ac9e8f94a09
Afroz3 <afrozmohammad1915@gmail.com>
2025-12-24 11:32:03 +0530

    Add print statements and comments to demonstrate Python syntax

--- syntax.py
+++ syntax.py
@@ -1,1 +1,14 @@
 print('hello world')
+#python indentation
+#indentation refers to spaces at the beginning of the code line
+#both single quotes and single quotes work in print statement in python
+#If you wat to print multiple line in same line use end parameter
+print('hello', end=" ")
+print('This will also print in the same line')
+#printing numbers
+print(1)
+#you can also do math operations inside print statement
+print(2+3)
+#you can also mix text and numbers in print statement using comma
+print("I am",22,"years old")
+#comments are used to explain a code and it is ignored by the python interpreter
