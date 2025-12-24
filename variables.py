26a6e2f73a888a8c9f87cd6acd99644fe7a32b7c
Afroz3 <afrozmohammad1915@gmail.com>
2025-12-24 20:19:53 +0530

    add variables.py file

--- variables.py
+++ variables.py
@@ -19,3 +19,37 @@ a =4
 A = 5
 print(a)
 print(A)
+#assigning multiple values to multiple variables
+x,y,z = "Orange", "Custard", "Mango"
+print(x)
+print(y)
+print(z)
+#assigning same value to multiple variables
+a = b = c = "Agent"
+print(a)
+print(b)
+print(c)
+#extracting values from a list or tuple is called unpacking
+cars = ["cooper", "ford", "tata"]
+c,e,f = cars
+print(c)
+print(e)
+print(f)
+#output variables
+x ="iam"
+y="learning"
+z="pyhton"
+print(x,y,z)
+#global variables
+#varaibles which are create outside a function are called global variables
+s = "awesome"
+def func():
+    print("python is", + s)
+func()
+#to create a global variable inside a function use global keyword
+def var():
+    global t
+    t ='fantastic'
+    print('python is ', + t)
+var()
+print('its' + t)
