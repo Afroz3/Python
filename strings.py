ac3b2baa1ccd6c01888cdee02791317e6db4b795
Afroz3 <2103a51026@sru.edu.in>
2025-12-25 11:39:11 +0530

    strings

---
+++ strings.py
@@ -0,0 +1,7 @@
+#strings are created using single or double quotes
+single_quote = 'Hello'
+double_quote = "world"
+#multiple line strings can be created using triple quotes
+multiline = '''this is a
+multi-line
+code in python'''
