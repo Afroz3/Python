8c5d796b0c6e0f7dda35c1d3500d63fe528a6043
Afroz3 <2103a51026@sru.edu.in>
2026-01-01 22:59:19 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,43 @@
+#operators are used to perform operations on variables and values
+#1 Arithmetic operators
+a = 10
+b = 20
+#addition
+print(a+b)
+#subtraction
+print(b-a)
+#multiplication
+print(a*b)
+#division
+print(b/a)
+#modulus
+print(b%a)
+#exponentiation
+print(a**3)
+#floor division
+print(b//a)
+#2 ASSIGNMENT OPERATORS
+x = 5
+x += 10 # x = x+10
+print(x)
+x -= 10 #x = x -10
+print(x)
+x *= 2 #x = x*2
+print(x)
+x /= 5 #x = x/5
+print(x)
+x %= 5
+print(x)
+x **= 3
+print(x)
+x //= 2
+print(x)
+#3 comparision operators
+# equals to ==
+#less than <
+#greater than >
+#less than or equal to <=
+#greater than or equal to >=
+#not equal to !=
+#logical operators
+#and or not
