c6c1b2261c1bd0a948e1a8dbff0aeecfd56c8099
Afroz3 <2103a51026@sru.edu.in>
2026-01-06 22:19:29 +0530

    ifelse

---
+++ ifelse.py
@@ -0,0 +1,19 @@
+#python supports the usual logical conditions from matheatics:
+#equals: a == b
+#not equals: a != b
+#less than: a<b
+#less than or equal to: a <= b
+#greater than: a>b
+#greater than or equal to: a >= b
+#these conditions can be used in several ways, most commonly in "if statements" and loops
+a =33
+b = 200
+if b>a:
+    print('b is greater than a')
+else:
+    print(' a is greater than b')
+number = 15
+if number < 10:
+    print('number is less than 10')
+else:
+    print('number is greater than or equal to 10')
