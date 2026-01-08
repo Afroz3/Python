16d1d30210855e3116bf6e4dedbc68153533cf3c
Afroz3 <2103a51026@sru.edu.in>
2026-01-08 22:51:29 +0530

    ifelse

--- ifelse.py
+++ ifelse.py
@@ -17,3 +17,46 @@ if number < 10:
     print('number is less than 10')
 else:
     print('number is greater than or equal to 10')
+#program to check if a person is eligible to vote
+'''age = int(input('enter you age :'))
+if age >= 18:
+    print('you are eligible')
+else:
+    print('NA')'''
+#program to check if a number is even or odd
+'''n = int(input('enter a number :'))
+if n % 2 == 0:
+    print('even')
+else:
+    print('Odd')'''
+#program to check if a number is divisible by 7
+'''n = int(input('enter a number :'))
+if n % 7 == 0:
+    print('divisible')
+else:
+    print('not divisible')
+n = int(input('enter a number :'))
+if n % 5 == 0:
+    print('Hello')
+else:
+    print('Bye')'''
+#program to calculate the electricity bill
+'''amt = 0
+units = input('enter your units :')
+if units <= 100:
+    amt = 0
+elif units > 100 and units <= 200:
+    amt = (units-100)*5
+else:
+    amt = 500+(units-200)*10
+print(amt)'''
+'''n = int(input('enter a number :'))
+lastnum = 0
+if n >= 10:
+    lastnum = n % 10
+else:
+    lastnum = 'invalid'
+print('last num is',lastnum)'''
+l = [1,2,3,4,5,True]
+l.pop(True)
+print(l)
