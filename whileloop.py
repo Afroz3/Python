7cfdea1dc39a3efd8f865e680b4caff07445463a
Afroz3 <2103a51026@sru.edu.in>
2026-01-09 22:35:43 +0530

    whileloop

---
+++ whileloop.py
@@ -0,0 +1,7 @@
+#python has two primitive loop commands
+#while and for
+#with while loop we can execute a set of statements as long as condition is true
+i = 1
+while i < 6:
+    print(i)
+    i+=1
