cdfdd0260a3acb251eaa2437688b75829aae56fb
Afroz3 <2103a51026@sru.edu.in>
2026-01-10 22:42:08 +0530

    forloop

---
+++ forloop.py
@@ -0,0 +1,29 @@
+#for loop is used for iterating over a sequence
+#with for loop we can execute set of statements, once for each item in a list, tuple or set
+fruits = ['apple', 'banana', 'carrot']
+'''for ch in fruits:
+    print(ch)'''
+#looping through elements in list
+'''for ch in 'banana':
+    print(ch)'''
+'''for ch in fruits:
+    print(ch)
+    if ch == 'banana':
+        break
+    print(ch)
+'''
+'''for ch in fruits:
+    if ch == 'banana':
+        continue
+    print(ch)'''
+'''for ch in range(6):
+    print(ch)
+for ch in range(2,7):
+    print(ch)'''
+'''for ch in range(0,100,5):
+    print(ch)'''
+#else in for loop
+'''for ch in range(6):
+    print(ch)
+else:
+    print('finished')'''
