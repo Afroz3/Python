8aa764d104f865dd3d8acee59cee3dd84d7a7ace
Afroz3 <2103a51026@sru.edu.in>
2026-01-10 20:56:05 +0530

    while

--- whileloop.py
+++ whileloop.py
@@ -1,7 +1,32 @@
 #python has two primitive loop commands
 #while and for
 #with while loop we can execute a set of statements as long as condition is true
-i = 1
+'''i = 1
 while i < 6:
     print(i)
-    i+=1
+    i+=1'''
+#break statement
+#with break statement we can stop the loop even the condition is true
+'''i = 1
+while i < 6:
+    print(i)
+    i += 1
+    if i == 3:
+        break
+    i += 1'''
+'''i = 0
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i)'''
+#else statement
+#else statement runs a block of code once condition is no longer true
+'''i = 7
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print('i is no longer less than 6')'''
+
+
