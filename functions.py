4883fa1167e0d622528ee36523a3fb8973ced0e2
Afroz3 <2103a51026@sru.edu.in>
2026-01-14 23:04:55 +0530

    functions

---
+++ functions.py
@@ -0,0 +1,9 @@
+#function is block of code which runs when it is called
+#function helps code repetition
+#creating a function
+def func():
+    print('hello from a function')
+func()
+func()
+func()
+
