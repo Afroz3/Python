0e6a842828ba8656ac8c77d5de2c049f1df0b8c1
Afroz3 <2103a51026@sru.edu.in>
2026-01-15 23:08:32 +0530

    functions

--- functions.py
+++ functions.py
@@ -1,9 +1,24 @@
-#function is block of code which runs when it is called
+'''#function is block of code which runs when it is called
 #function helps code repetition
 #creating a function
 def func():
     print('hello from a function')
 func()
 func()
-func()
-
+func()'''
+#why to use functions
+'''def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit-32)* 5/9
+print(fahrenheit_to_celsius(96.8))'''
+#return values
+'''def get_greeting():
+    return 'hello'
+message=get_greeting()
+print(message)'''
+'''def greet():
+    return 'hello from a function'
+print(greet())'''
+#pass statement
+#Arguments in functions
+#information can be passed to functions as arguments
+#arguments are specified after the function name 
