9b2c9a849cce1151225965ad2080c5bc7784d266
Afroz3 <2103a51026@sru.edu.in>
2026-01-18 22:26:06 +0530

    arrays

---
+++ arrays.py
@@ -0,0 +1,9 @@
+#Arrays are used to store multiple values in one single variable
+cars = ['ford','tata','kia']
+#an array is a special variable which can hold more than one value at a time
+print(cars[0])
+cars[0] = 'toyota'
+print(cars)
+print(len(cars))
+cars.append('honda')
+print(cars)
