748d3bcd76d0066c0ed187f6059869fe0a20883b
Afroz3 <2103a51026@sru.edu.in>
2026-01-19 23:04:27 +0530

    math methods

---
+++ math medthod.py
@@ -0,0 +1,12 @@
+#math method
+#built in math function
+'''x = min(5,10,15)
+y = max(5,10,15)
+print(x,y)
+x = abs(-100.65)
+print(x)'''
+'''x = pow(3,3)
+print(x)
+'''
+#importing math method
+import math
